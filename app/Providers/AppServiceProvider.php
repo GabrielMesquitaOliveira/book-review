843e49e069ea0b69b8dc175b05298494a54e61a7
gabriel <gabrielmesquita0503@gmail.com>
2024-11-18 15:26:49 -0300

    Rate Limiting

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -18,7 +21,14 @@ class AppServiceProvider extends ServiceProvider
      * Bootstrap any application services.
      */
     public function boot(): void
+
     {
-        //
+
+        RateLimiter::for('reviews', function (Request $request) {
+
+           return Limit::perHour(3)->by(optional($request->user())->id ?: $request->ip());
+
+        });
+
     }
 }
